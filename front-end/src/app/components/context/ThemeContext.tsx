31247ea148748bf48d460e02455a67aad0737a63
ManikandanP24 <manikandanvj24@gamil.com>
2024-02-08 12:40:15 +0530

    new update

--- front-end/src/app/components/context/ThemeContext.tsx
+++ front-end/src/app/components/context/ThemeContext.tsx
@@ -3,6 +3,8 @@ import React, { createContext, useState } from "react";
 
 export const ThemeContext = createContext();
 
+// console.log("..........",ThemeContext)
+
 interface ThemeProviderProps {
   children: React.ReactNode;
 }
